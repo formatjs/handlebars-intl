0abb08788ea1b5cc2eaea448f356d31f09f7db02
Anthony Pipkin <a.pipkin@yahoo.com>
2014-01-16 15:24:29 -0500

    Clean up ignores and description.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,8 +18,6 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadTasks('./tasks');
-
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
